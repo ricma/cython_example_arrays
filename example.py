04ef898dfb1d961c742c6e43ae065c37b6920b06
Martin Richter <martin.richter@nottingham.ac.uk>
2020-02-18 16:29:57 +0000

    add debug versions of module

--- example.py
+++ example.py
@@ -4,6 +4,9 @@ An example calling the cython wrapper
 import numpy as np
 from calculate import example_calculate
 
+# This is the version compiled with debug symbols and w/o optimizations
+from calculate_dbg import example_calculate as example_calculate_dbg
+
 
 def example_main():
     """
@@ -13,7 +16,14 @@ def example_main():
     input_data_1d = np.array([1, 2, 3])
     input_data_2d = np.array([[10, 20], [30, 40]])
 
-    output_2d, output_1d = example_calculate(
+    # FIXME: We call the dbg version for now
+    # To use that:
+    # % ulimit -c unlimited
+    # % python3 example.py
+    #   --> should core dump
+    # % gdb python3 core
+    # should start the debugger and get you where the exception happened.
+    output_2d, output_1d = example_calculate_dbg(
         input_data_2d,
         input_data_1d)
 
